1c7e805e237a0029d6bb38e2d9535d9dc0292fdd
rich2025 <richy@bu.edu>
2024-09-08 21:38:30 -0400

    added oauth

--- code/Exercise_03/frontend/src/App.jsx
+++ code/Exercise_03/frontend/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import LoginButton from "./components/login";
+import LogoutButton from "./components/logout";
+import { useEffect } from 'react';
+import { gapi } from 'gapi-script';
+
+const clientId = "819894863579-nlh5evpomia8q6h7mn1h2j5m1ocpetl7.apps.googleusercontent.com";
 
 function App() {
   
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientId,
+        scope: ""
+      })
+    };
+    gapi.load('client:auth2', start);
+  })
+
   return (
     <>
       <div>
-        
+      <h1>Response Time Game</h1>
+      <LoginButton />
+      <LogoutButton />
    
       </div>
-      <h1>Response Time Game</h1>
       
     </>
   )
--- code/Exercise_03/frontend/src/components/login.jsx
+++ code/Exercise_03/frontend/src/components/login.jsx
@@ -1,4 +1,6 @@
-import { GoogleLogin } from 'react-google-login'
+import { GoogleLogin } from '@leecheuk/react-google-login';
+
+// with reference to https://www.youtube.com/watch?v=HtJKUQXmtok&t=172s
 
 const clientID = "819894863579-nlh5evpomia8q6h7mn1h2j5m1ocpetl7.apps.googleusercontent.com";
 
